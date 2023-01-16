9a62a14590d239a3d8f2adc8b1361ecddd8d99bc
anowereng <anowereng@gmail.com>
2023-01-16 19:18:14 +0600

    ParallelOptions add for max download

--- M2SysAssesment/Controllers/ImageController.cs
+++ M2SysAssesment/Controllers/ImageController.cs
@@ -20,58 +20,77 @@ namespace M2SysAssesment.Controllers
         {
             var responseModel = new ResponseDownload();
 
-          var urls = new List<string>()
+            var urls = new List<string>()
             {
-                "https://images.unsplash.com/photo-1525923838299-2312b60f6d69"
-                //"https://i.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4",
-                //"https://i.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ",
-                //"https://i.picsum.photos/id/3/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ"
+                "https://i.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4",
+                "https://i.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4",
+                "https://i.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ",
+                "https://i.picsum.photos/id/1/5000/3333.jpg?hmac=Asv2DU3rA_5D1xSe22xZK47WEAN0wjWeFOhzd13ujW4"
             };
 
             await Download(urls);
             return Ok(responseModel);
         }
 
-        private async Task Download(IEnumerable<string> urls)
+        private async Task<bool> Download(IEnumerable<string> urls)
         {
-            //Console.WriteLine("Start now");
-            var localPath = "";
-
             var path = Path.Combine("wwwroot/images");
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
-            //var fullFileName = Path.Combine("wwwroot", subCategoryName + "//" + webRootPath, fileName);
 
-            var urlTasks = urls.Select(async (url, index) =>
+            var client = new HttpClient();
+            var options = new ParallelOptions() { MaxDegreeOfParallelism = 1 };
+            // now let's send HTTP requests to each of these URLs in parallel
+            await Parallel.ForEachAsync(urls, options, async (url, cancellationToken) =>
             {
-                string ext = System.IO.Path.GetExtension(url);
-                using (var client = new HttpClient())
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+                // Add our custom headers
+                requestMessage.Headers.Add("User-Agent", "Other");
+
+                using (var response = await client.SendAsync(requestMessage))
                 {
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    //client.DefaultRequestHeaders.Add("User-Agent", "Other");
-                    using (var responseStream = await client.GetStreamAsync(url))
+                    if (response.IsSuccessStatusCode)
                     {
-                        //if (!response.IsSuccessStatusCode)
-                        //    return (url, null);
-                        //var data = await response.st.ReadAsByteArrayAsync().ConfigureAwait(false);
-                        //using var fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
-                        //response.CopyTo(fileStream);
-
                         var fileName = $"{Guid.NewGuid().ToString()}.jpg";
-                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", fileName);
-                        using (var fileStream = new FileStream(filePath, FileMode.Create))
-                        {
-                            await responseStream.CopyToAsync(fileStream);
-                        }
 
-                        return (url, fileName);
+                        var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", fileName);
+
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                            await response.Content.CopyToAsync(fileStream);
                     }
-                };
+
+                }
             });
-            await Task.WhenAll(urlTasks);
-            //Console.WriteLine("Done");
-            //return urlTasks.Select(x => x.Result).ToArray();
+
+            return true;
+
+            //var urlTasks = urls.Select(async (url, index) =>
+            //{
+            //    string ext = Path.GetExtension(url);
+            //    using (var client = new HttpClient())
+            //    {
+            //        HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            //        // Add our custom headers
+            //        requestMessage.Headers.Add("User-Agent", "Other");
+
+            //        using (var response = await client.SendAsync(requestMessage))
+            //        {
+            //            if (!response.IsSuccessStatusCode)
+            //                return (url, null);
+
+            //            var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+
+            //            var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\images", fileName);
+
+            //            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            //                await response.Content.CopyToAsync(fileStream);
+
+            //            return (url, fileName);
+            //        }
+            //    };
+            //});
+            //await Task.WhenAll(urlTasks);
+
         }
 
     }
